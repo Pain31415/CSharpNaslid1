04814e73ba92869ec3496c84d1c96820d9fa731c
Pain31415 <Cherniy412@ukr.net>
2024-03-01 20:11:12 +0200

    Add project files.

---
+++ CSharpNaslid1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GeometricShapes;
+
+namespace GeometricShapesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5, 10);
+
+            double rectangleArea = rectangle.CalculateArea();
+
+            Console.WriteLine($"Площа прямокутника: {rectangleArea}");
+        }
+    }
+
+}
---
+++ GeometricShapes/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes
+{
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+    }
+}
